c268a2e71882005e493d4f52bf6167d2a4413a66
kristof1345 <kristofnemeth1345@gmail.com>
2023-05-11 18:33:43 +0200

    nvlk

--- server/main.go
+++ server/main.go
@@ -19,7 +19,7 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:3000/",
+		AllowOrigins: "http://localhost:3000",
 		AllowHeaders: "Origin, Content-Type, Accept",
 	}))
 
